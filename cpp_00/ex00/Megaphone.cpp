0baaf479fc1c89617934e964e99b10870dcbf4df
Julien Dupuis <jdupuis@c2r1p9.42perpignan.fr>
2025-10-02 17:51:41 +0200

    CPP00/ex00 - auto/dev

--- cpp_00/ex00/Megaphone.cpp
+++ cpp_00/ex00/Megaphone.cpp
@@ -1,41 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 20:16:00 by jdupuis           #+#    #+#             */
-/*   Updated: 2025/10/02 16:22:50 by jdupuis          ###   ########.fr       */
+/*   Updated: 2025/10/02 17:50:38 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <cctype>
 
-int main( int argc, char **argv )
-{
-	int i = 1;
-	std::string	str;
-	
-	if (argc < 2)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
-	}
-	while (argv[i])
-	{
-		str = argv[i];
-		transform(str.begin(), str.end(), str.begin(), toupper);
-		std::cout << str;
-		i++;
-	}
-	std::cout << std::endl;
-	return (0);
-}
-/*
 int main(int argc, char **argv)
 {
 	int		i;
@@ -60,4 +37,3 @@ int main(int argc, char **argv)
 	std::cout << std::endl;
 	return (0);
 }
-*/
