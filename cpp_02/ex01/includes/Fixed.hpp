4513496465d9a550e26dc3087d626c2c95d6ac1c
jdupuis <jdupuis@student.42perpignan.fr>
2025-10-12 17:57:09 +0200

    CPP02/ex01 - auto/dev

--- cpp_02/ex01/includes/Fixed.hpp
+++ cpp_02/ex01/includes/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 20:57:28 by jdupuis           #+#    #+#             */
-/*   Updated: 2025/10/07 21:21:58 by jdupuis          ###   ########.fr       */
+/*   Updated: 2025/10/12 17:55:33 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,27 @@
 
 #include <iostream>
 
-class	Fixed
+class Fixed
 {
-	public:
-		Fixed();
-		~Fixed();
-		int	toInt();
-		
 	private:
-};
+		int	fixed_point;
+		static const int bits = 8;
+
+	public:
+		Fixed();								// Canonical : Constructor
+		Fixed( const int n );
+		Fixed( const float n );
+		Fixed( Fixed const& other );			// Canonical : Constructor
+		~Fixed();								// Canonical : Destructor
+		Fixed & operator=( Fixed const & a );	// Canonical : Assign Operator '=' to Fixed
+
+		int		getRawBits( void ) const;
+		void	setRawBits( int const data );
+		
+		int		toInt( void ) const;
+		float	toFloat( void ) const;
+	};
+
+std::ostream& operator<<( std::ostream& os, Fixed const& a );
 
 #endif
--- cpp_02/ex01/src/Fixed.cpp
+++ cpp_02/ex01/src/Fixed.cpp
@@ -6,22 +6,70 @@
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 20:57:20 by jdupuis           #+#    #+#             */
-/*   Updated: 2025/10/07 21:23:26 by jdupuis          ###   ########.fr       */
+/*   Updated: 2025/10/12 17:56:51 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Fixed.hpp"
 
-Fixed::Fixed()
+Fixed& Fixed::operator=( Fixed const& a )
 {
-	std::cout << "Constructor called" << std::endl;
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->fixed_point = a.getRawBits();
+	return (*this);
 }
+
+std::ostream& operator<<(std::ostream& os, Fixed const& a)
+{
+	os << a.toFloat();
+	return (os);
+}
+
+Fixed::Fixed( void ) : fixed_point (0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed( const int n )
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->fixed_point = n << this->bits;
+}
+
+Fixed::Fixed( const float n )
+{
+	std::cout << "Float constructor called" << std::endl;
+	float scaled = n * ( 1 << this->bits );
+	this->fixed_point = (int)(scaled + (scaled >= 0 ? 0.5f : -0.5f));
+}
+
+Fixed::Fixed( Fixed const& other )
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
 }
 
-int	Fixed::toInt()
+int	Fixed::getRawBits( void ) const
 {
-	return ();
+	return (this->fixed_point);
+}
+
+void	Fixed::setRawBits( int const data )
+{
+	this->fixed_point = data;
+}
+
+int	Fixed::toInt( void ) const
+{
+	return ( (int)this->fixed_point >> this->bits );
+}
+
+float	Fixed::toFloat( void ) const
+{
+	return ( (float)this->fixed_point / (1 << this->bits) );
 }
