72f348a95938c2c4f78552bd7a53bcff7e7b1869
jdupuis <jdupuis@student.42perpignan.fr>
2025-10-13 16:00:19 +0200

    CPP02/ex02 - auto/dev

--- cpp_02/ex02/includes/Fixed.hpp
+++ cpp_02/ex02/includes/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 20:57:28 by jdupuis           #+#    #+#             */
-/*   Updated: 2025/10/07 21:10:24 by jdupuis          ###   ########.fr       */
+/*   Updated: 2025/10/13 15:58:48 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,51 @@
 # define FIXED_HPP
 
 #include <iostream>
+#include <cmath>
 
-class	Fixed
+class Fixed
 {
-	public:
-		Fixed();
-		~Fixed();
-		
 	private:
+		int	_fixed_point;
+		static const int _bits = 8;
+
+	public:
+		~Fixed();								// Canonical : Destructor
+		Fixed();								// Canonical : Constructor
+		Fixed( const int n );
+		Fixed( const float n );
+		Fixed( Fixed const& other );			// Canonical : Constructor
+		Fixed & operator=( Fixed const & a );	// Canonical : Assign Operator '=' to Fixed
+		
+		bool operator>( Fixed const & a ) const;
+		bool operator<( Fixed const & a ) const;
+		bool operator>=( Fixed const & a) const;
+		bool operator<=( Fixed const & a ) const;
+		bool operator==( Fixed const & a ) const;
+		bool operator!=( Fixed const & a ) const;
+		
+		Fixed operator+( Fixed const & a ) const;
+		Fixed operator-( Fixed const & a ) const;
+		Fixed operator*( Fixed const & a ) const;
+		Fixed operator/( Fixed const & a ) const;
+
+		Fixed & operator++( void );
+		Fixed operator++( int );
+		Fixed & operator--( void );
+		Fixed operator--( int );
+		
+		int		getRaw_bits( void ) const;
+		void	setRaw_bits( int const data );
+		
+		int		toInt( void ) const;
+		float	toFloat( void ) const;
+		
+		static	Fixed const&	min(Fixed const& a, Fixed const& b);
+		static	Fixed const&	max(Fixed const& a, Fixed const& b);
+		static	Fixed&			min(Fixed& a, Fixed& b);
+		static	Fixed&			max(Fixed& a, Fixed& b);
 };
+
+std::ostream& operator<<( std::ostream& os, Fixed const& a );
 
 #endif
--- cpp_02/ex02/src/Fixed.cpp
+++ cpp_02/ex02/src/Fixed.cpp
@@ -6,17 +6,186 @@
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 20:57:20 by jdupuis           #+#    #+#             */
-/*   Updated: 2025/10/07 21:10:33 by jdupuis          ###   ########.fr       */
+/*   Updated: 2025/10/13 15:58:48 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Fixed.hpp"
 
-Fixed::Fixed()
+Fixed::Fixed( void ) : _fixed_point ( 0 )
 {
-	std::cout << "Constructor called" << std::endl;
+	// std::cout << "Default constructor called" << std::endl;
 }
-Fixed::~Fixed()
+
+Fixed::Fixed( const int n )
 {
-	std::cout << "Destructor called" << std::endl;
+	// std::cout << "Int constructor called" << std::endl;
+	this->_fixed_point = n << this->_bits;
+}
+
+Fixed::Fixed( const float n ) : _fixed_point ( roundf(n * (1 << this->_bits)) )
+{
+	// std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::Fixed( Fixed const& other )
+{
+	// std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed::~Fixed( void )
+{
+	// std::cout << "Destructor called" << std::endl;
+}
+
+Fixed&	Fixed::operator=( Fixed const& a )
+{
+	// std::cout << "Copy assignment operator called" << std::endl;
+	this->_fixed_point = a.getRaw_bits();
+	return ( *this );
+}
+
+std::ostream&	operator<<( std::ostream& os, Fixed const& a )
+{
+	os << a.toFloat();
+	return ( os );
+}
+
+bool	Fixed::operator>( Fixed const& a ) const
+{
+	return ( this->_fixed_point > a.getRaw_bits() );
+}
+
+bool	Fixed::operator<( Fixed const& a ) const
+{
+	return ( this->_fixed_point < a.getRaw_bits() );
+}
+
+bool	Fixed::operator<=( Fixed const& a ) const
+{
+	return ( this->_fixed_point <= a.getRaw_bits() );
+}
+
+bool	Fixed::operator>=( Fixed const& a ) const
+{
+	return ( this->_fixed_point >= a.getRaw_bits() );
+}
+
+bool	Fixed::operator==( Fixed const& a ) const
+{
+	return ( this->_fixed_point == a.getRaw_bits() );
+}
+
+bool	Fixed::operator!=( Fixed const& a ) const
+{
+	return ( this->_fixed_point != a.getRaw_bits() );
+}
+
+Fixed	Fixed::operator+( Fixed const& a ) const
+{
+	Fixed	newFixed;
+
+	newFixed.setRaw_bits( this->_fixed_point + a._fixed_point );
+	return ( newFixed );
+}
+
+Fixed 	Fixed::operator-( Fixed const& a ) const
+{
+	Fixed	newFixed;
+
+	newFixed.setRaw_bits( this->_fixed_point - a._fixed_point );
+	return ( newFixed );
+}
+
+Fixed 	Fixed::operator*( Fixed const& a ) const
+{
+	Fixed	newFixed;
+
+	newFixed.setRaw_bits( (this->_fixed_point * a._fixed_point) >> this->_bits);
+	return ( newFixed );
+}
+
+Fixed	Fixed::operator/( Fixed const& a ) const
+{
+	Fixed	newFixed;
+
+	newFixed.setRaw_bits( (this->_fixed_point << this->_bits) / a._fixed_point );
+	return ( newFixed );
+}
+
+Fixed& 	Fixed::operator++( void )
+{
+	this->_fixed_point++;
+	return ( *this );
+}
+
+Fixed	 Fixed::operator++( int )
+{
+	Fixed	newFixed( *this );
+	
+	this->_fixed_point++;
+	return ( newFixed );
+}
+
+Fixed&	Fixed::operator--( void )
+{
+	this->_fixed_point--;
+	return ( *this );
+}
+
+Fixed	Fixed::operator--( int )
+{
+	Fixed	newFixed( *this );
+	
+	this->_fixed_point--;
+	return ( newFixed );
+}
+
+int	Fixed::getRaw_bits( void ) const
+{
+	return ( this->_fixed_point );
+}
+
+void	Fixed::setRaw_bits( int const data )
+{
+	this->_fixed_point = data;
+}
+
+int	Fixed::toInt( void ) const
+{
+	return ( (int)this->_fixed_point >> this->_bits );
+}
+
+float	Fixed::toFloat( void ) const
+{
+	return ( (float)this->_fixed_point / (1 << this->_bits) );
+}
+
+Fixed const	&Fixed::max( Fixed const& a, Fixed const& b )
+{
+	if ( a._fixed_point > b._fixed_point )
+		return ( a );
+	return ( b );
+}
+
+Fixed	&Fixed::max( Fixed &a, Fixed &b )
+{
+	if ( a._fixed_point > b._fixed_point )
+		return ( a );
+	return ( b );
+}
+
+Fixed const	&Fixed::min( Fixed const& a, Fixed const& b )
+{
+	if ( a._fixed_point < b._fixed_point )
+		return ( a );
+	return ( b );
+}
+
+Fixed	&Fixed::min( Fixed &a, Fixed &b )
+{
+	if ( a._fixed_point < b._fixed_point )
+		return ( a );
+	return ( b );
 }
