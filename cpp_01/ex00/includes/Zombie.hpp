e127b7f1076fa0269f89e7d446b3a099560007a3
Julien <ju@MacBook-Air-de-Ju.local>
2025-10-15 16:31:32 +0200

    CPP01/ex00 - auto/dev

--- cpp_01/ex00/includes/Zombie.hpp
+++ cpp_01/ex00/includes/Zombie.hpp
@@ -6,12 +6,12 @@
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 12:40:16 by jdupuis           #+#    #+#             */
-/*   Updated: 2025/10/13 17:48:26 by jdupuis          ###   ########.fr       */
+/*   Updated: 2025/10/15 16:29:04 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ZOMBIEhp_P
-# define ZOMBIEhp_P
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
 
 #include <iostream>
 
