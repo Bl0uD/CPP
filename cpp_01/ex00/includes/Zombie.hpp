1b7659591b64fb0abf4ccd7f3902c879a84e9a49
jdupuis <jdupuis@student.42perpignan.fr>
2025-10-16 15:20:02 +0200

    CPP01/ex00 - auto/dev

--- cpp_01/ex00/includes/Zombie.hpp
+++ cpp_01/ex00/includes/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 12:40:16 by jdupuis           #+#    #+#             */
-/*   Updated: 2025/10/15 16:29:04 by jdupuis          ###   ########.fr       */
+/*   Updated: 2025/10/16 15:19:20 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ class Zombie
 		void	announce( void );
 		
 	private:
-		std::string	name_;
+		std::string	name;
 };
 
 void	randomChump( std::string name );
--- cpp_01/ex00/src/Zombie.cpp
+++ cpp_01/ex00/src/Zombie.cpp
@@ -6,24 +6,24 @@
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 12:40:31 by jdupuis           #+#    #+#             */
-/*   Updated: 2025/10/13 17:48:26 by jdupuis          ###   ########.fr       */
+/*   Updated: 2025/10/16 15:19:33 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Zombie.hpp"
 
-Zombie::Zombie(std::string name) : name_( name )
+Zombie::Zombie(std::string name) : name( name )
 {
-	std::cout << "Zombie " << this->name_ << " has spawned !" << std::endl;
+	std::cout << "Zombie " << this->name << " has spawned !" << std::endl;
 }
 
 Zombie::~Zombie( void )
 {
-	std::cout << "Zombie " << this->name_ << " is dead... 💀" << std::endl;
+	std::cout << "Zombie " << this->name << " is dead... 💀" << std::endl;
 	return ;
 }
 
 void	Zombie::announce( void )
 {
-	std::cout << name_ << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
