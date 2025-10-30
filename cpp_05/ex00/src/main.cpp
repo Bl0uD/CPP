bdbc445652cb03682cc18c8a4eec3aeb264948a6
Julien <ju@macbook-air-de-ju.home>
2025-10-30 13:33:52 +0100

    CPP05/ex00 - auto/dev

--- cpp_05/ex00/src/main.cpp
+++ cpp_05/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 12:12:45 by jdupuis           #+#    #+#             */
-/*   Updated: 2025/10/29 13:51:21 by jdupuis          ###   ########.fr       */
+/*   Updated: 2025/10/30 13:33:09 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,61 @@
 
 int	main( void )
 {
-	try {
-		Bureaucrat alice("Alice", 1);
-		// std::cout << alice << std::endl; // in case you want to test "<<"
-		alice.incrementGrade();				// Should throw an exception
-	} catch (const std::exception &e) {
+	try
+	{
+		Bureaucrat alice("Alice", 12);
+		std::cout << alice << std::endl;
+		alice.incrementGrade();
+	}
+	catch (const std::exception &e)
+	{
 		std::cerr << "Exception: " << e.what() << std::endl;
 	}
 
-	std::cout << "\n";
-	try {
+	std::cout << std::endl;
+	try
+	{
 		Bureaucrat bob("Bob", 150);
-		bob.decrementGrade();				// Should throw an exception
-	} catch (const std::exception &e) {
+		bob.decrementGrade();
+	}
+	catch (const std::exception &e)
+	{
 		std::cerr << "Exception: " << e.what() << std::endl;
 	}
-	try {
-		Bureaucrat charlie("Charlie", 0);	// Should throw an exception
-	} catch (const std::exception &e) {
+	try
+	{
+		Bureaucrat charlie("Charlie", 0);
+	}
+	catch (const std::exception &e)
+	{
 		std::cerr << "Exception: " << e.what() << std::endl;
 	}
 
-	std::cout << "\n";
-	try {
-		Bureaucrat diana("Diana", 151);		// Should throw an exception
-	} catch (const std::exception &e) {
+	std::cout << std::endl;
+	try
+	{
+		Bureaucrat diana("Diana", 151);
+	}
+	catch (const std::exception &e)
+	{
 		std::cerr << "Exception: " << e.what() << std::endl;
 	}
 
-	std::cout << "\n";
-	try {
+	std::cout << std::endl;
+	try
+	{
 		Bureaucrat eve("Eve", 5);
-		eve.incrementGrade();				// 4
-		eve.incrementGrade();				// 3
-		eve.incrementGrade();				// 2
-		eve.incrementGrade();				// 1
-		eve.incrementGrade();				// will throw error here. and stop try process.
+		eve.incrementGrade();
+		eve.incrementGrade();
+		eve.incrementGrade();
+		eve.incrementGrade();
+		eve.incrementGrade();
 		eve.incrementGrade();
 		eve.incrementGrade();
 		eve.decrementGrade();
-	} catch (const std::exception &e) {
+	}
+	catch (const std::exception &e)
+	{
 		std::cerr << "Exception: " << e.what() << std::endl;
 	}
 
