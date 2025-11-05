a030351224aae64e2c5cf8896c5119860cf7a1f8
jdupuis <jdupuis@student.42perpignan.fr>
2025-11-05 19:44:26 +0100

    CPP05/ex02 - auto/dev

--- cpp_05/ex02/includes/AForm.hpp
+++ cpp_05/ex02/includes/AForm.hpp
@@ -1,12 +1,66 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Aform.hpp                                          :+:      :+:    :+:   */
+/*   AForm.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 16:01:16 by jdupuis           #+#    #+#             */
-/*   Updated: 2025/10/30 16:01:17 by jdupuis          ###   ########.fr       */
+/*   Updated: 2025/10/31 22:00:59 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef AFORM_HPP
+# define AFORM_HPP
+
+class AForm
+{
+	private:
+		const std::string	_name;
+		bool 				_signed;
+		const int			_gradeToSign;
+		const int			_gradeToExec;
+		
+	public:
+		~AForm( void );
+		AForm( void );
+		AForm( std::string name, int gradeToSign, int gradeToExec );
+		AForm( Form const &copy );
+		Form &operator=( Form const &instance );
+
+		std::string getName( void ) const;
+		bool getSigned( void ) const;
+		int getGradeToSign( void ) const;
+		int getGradeToExec( void ) const;
+
+		void beSigned( Bureaucrat const &instance );
+		
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade is too high !!");
+				}
+		};
+		
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade is too low !!");
+				}
+		};
+
+		class AlreadySigned : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Already signed.");
+				}
+		};
+}
+
+#endif
