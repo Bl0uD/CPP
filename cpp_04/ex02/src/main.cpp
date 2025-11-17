b6407b14a30b0c1f9a8e77612584093cb4a99422
Julien Dupuis <jdupuis@c3r1p4.42perpignan.fr>
2025-11-17 17:18:39 +0100

    CPP04/ex02 - auto/dev

--- cpp_04/ex02/src/main.cpp
+++ cpp_04/ex02/src/main.cpp
@@ -6,36 +6,37 @@
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 14:56:54 by jdupuis           #+#    #+#             */
-/*   Updated: 2025/11/16 23:53:51 by jdupuis          ###   ########.fr       */
+/*   Updated: 2025/11/17 17:18:31 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Dog.hpp"
 #include "../includes/Cat.hpp"
 
-int    main( void )
+int	main( void )
 {
-    const AAnimal *tab[10];
-    int    i = 0;
+	/*const	AAnimal* meta = new AAnimal();*/	//test Abstract class (not instantiable)
+	const	AAnimal *tab[10];
+	int		i = 0;
 
-    while (i < 10)
-    {
-        if (!(i % 2))
-            tab[i] = new Cat("Sally");
-        else
-            tab[i] = new Dog("Doggy");
-        i++;
-        std::cout << std::endl;
-    }
+	while (i < 10)
+	{
+		if (!(i % 2))
+			tab[i] = new Cat();
+		else
+			tab[i] = new Dog();
+		i++;
+		std::cout << std::endl;
+	}
 
-    std::cout << std::endl << std::endl;
+	std::cout << std::endl << std::endl;
 
-    i = 0;
-    while (i < 10)
-    {
-        delete(tab[i]);
-        i++;
-        std::cout << std::endl;
-    }
+	i = 0;
+	while (i < 10)
+	{
+		delete(tab[i]);
+		i++;
+		std::cout << std::endl;
+	}
 	return (0);
 }
