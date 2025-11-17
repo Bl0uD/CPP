6fce29fe53f9a6f9caed580b7f54a7945f917eeb
Julien Dupuis <jdupuis@c3r1p4.42perpignan.fr>
2025-11-17 17:19:26 +0100

    CPP04/ex02 - auto/dev

--- cpp_04/ex02/src/Brain.cpp
+++ cpp_04/ex02/src/Brain.cpp
@@ -6,7 +6,7 @@
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 15:10:10 by jdupuis           #+#    #+#             */
-/*   Updated: 2025/11/17 00:18:19 by jdupuis          ###   ########.fr       */
+/*   Updated: 2025/11/17 17:19:21 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,9 @@ Brain const& Brain::operator=( Brain const & instance )
 {
 	int i = 0;
 	
-	if (this == &instance)
+	if ( this == &instance )
 		return ( *this );
-	while (i < 100)
+	while ( i < 100 )
 	{
 		this->_ideas[i] = instance._ideas[i];
 		i++;
