0deb2aa1d6e9538852c227b745c16bacc7f90ddf
Julien <ju@MacBook-Air-de-Ju.local>
2026-04-17 11:22:17 +0200

    CPP06/ex01 - auto/dev

--- cpp_06/ex01/src/main.cpp
+++ cpp_06/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 15:58:21 by jdupuis           #+#    #+#             */
-/*   Updated: 2026/01/09 16:40:18 by jdupuis          ###   ########.fr       */
+/*   Updated: 2026/04/17 11:15:56 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void printData( const Data* data, const std::string& label )
 	std::cout << "\n********** " << label << " **********" << std::endl;
 	std::cout << "->> x: " << data->x << std::endl;
 	std::cout << "->> y: " << data->y << std::endl;
-	std::cout << "->> z: " << data->y << std::endl;
+	std::cout << "->> z: " << data->z << std::endl;
 	std::cout << "->> id : " << data->id << std::endl;
 	std::cout << "->> seed : " << data->seed << std::endl;
 	std::cout << "->> password : " << data->password << std::endl;
@@ -38,10 +38,15 @@ int main( void )
 	printData( &data, "Before Serialization" );
 
 	uintptr_t raw = Serializer::serialize( &data );
-	std::cout << "\033[1;33m\nSerialized uintptr_t: " << "\033[0m" << raw << "  😎"  << std::endl;
+	std::cout << "\nSerialized uintptr_t: " << raw << std::endl;
 
 	Data* ptr = Serializer::deserialize( raw );
 	printData( ptr, "After Deserialization" );
+
+	if (ptr == &data)
+		std::cout << "\nPointer check: OK (deserialize returns original address)" << std::endl;
+	else
+		std::cout << "\nPointer check: KO" << std::endl;
 
 	return ( 0 );
 }
